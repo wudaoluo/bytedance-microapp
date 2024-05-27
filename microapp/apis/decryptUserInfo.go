d899e830dc4711ef8f101e75b37f368dcec276e5
王思远 <wangsiyuan@luojilab.com>
2024-05-27 16:55:08 +0800

    修改参数

--- microapp/apis/decryptUserInfo.go
+++ microapp/apis/decryptUserInfo.go
@@ -19,16 +19,20 @@ func DecryptUserInfo(encryptedData, sessionKey, iv string) DecryptUserInfoResp {
 	return resp
 }
 
+//获取手机号 和获取用户基本信息 是2个不同的功能，都放在一个结构体了
 type DecryptUserInfoResp struct {
-	NickName  string `json:"nickName"`
-	AvatarUrl string `json:"avatarUrl"`
-	Gender    int    `json:"gender"`   // 用户性别，0: 未知；1:男性；2:女性
-	City      string `json:"city"`     // 用户所在城市
-	Province  string `json:"province"` // 用户所在省份
-	Country   string `json:"country"`  //	用户所在国家
-	OpenId    string `json:"openId"`   //	用户 openId
-	Watermark struct {
+	NickName        string `json:"nickName"`
+	AvatarUrl       string `json:"avatarUrl"`
+	Gender          int    `json:"gender"`   // 用户性别，0: 未知；1:男性；2:女性
+	City            string `json:"city"`     // 用户所在城市
+	Province        string `json:"province"` // 用户所在省份
+	Country         string `json:"country"`  //	用户所在国家
+	OpenId          string `json:"openId"`   //	用户 openId
+	CountryCode     string `json:"countryCode"`
+	PhoneNumber     string `json:"phoneNumber"`
+	PurePhoneNumber string `json:"purePhoneNumber"`
+	Watermark       struct {
 		Appid     string `json:"appid"`
 		Timestamp int    `json:"timestamp"`
-	} //敏感数据水印
+	}                      //敏感数据水印
 }
